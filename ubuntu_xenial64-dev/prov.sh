c40c94be74cedfe4d9aafd2b92de7fecd45a8266
Alex Rodriguez <unc741@gmail.com>
2018-03-22 06:54:15 -0400

    adding some more good default stuff

--- ubuntu_xenial64-dev/prov.sh
+++ ubuntu_xenial64-dev/prov.sh
@@ -9,9 +9,12 @@ sudo -u vagrant sh -c 'echo set editing-mode vi >> /home/vagrant/.inputrc'
 ## or you can do this and just make a quick alias, that you call to setup your env
 # echo "alias dev='cd /coding/'" >> /home/vagrant/.bashrc
 
-### updates and installs
-## always update your box, because you want the most up to date packages
+## updates and installs
+# always update your box, because you want the most up to date packages
 sudo apt-get update
 
-## now do any of your installs here
-#sudo apt-get install
+# now do any of your installs here
+sudo apt-get install -y screen
+
+# adding some commone sense aliases
+echo "alias dev='cd /coding/'" >> /home/vagrant/.bashrc
