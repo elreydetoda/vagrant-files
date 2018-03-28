c5a1db96b57dc027de30595cdbc48b99cbe10590
elrey <unc741@gmail.com>
2018-03-27 20:03:11 -0400

    modularizing script a little more

--- ubuntu_xenial64-dev/prov.sh
+++ ubuntu_xenial64-dev/prov.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+U=vagrant
+
 # always set vim mode =)
-sudo -u vagrant sh -c 'echo set editing-mode vi >> /home/vagrant/.inputrc'
+sudo -u $U sh -c "echo set editing-mode vi >> /home/${U}/.inputrc"
 
 ## depending on your dev envionment you can set this to make it easy to access your code
 #ln -s /coding /home/vagrant/coding
@@ -17,4 +19,5 @@ sudo apt-get update
 sudo apt-get install -y screen
 
 # adding some commone sense aliases
-echo "alias dev='cd /coding/'" >> /home/vagrant/.bashrc
+echo "alias dev='cd /coding/'" >> /home/${U}/.bashrc
+echo "alias screen='screen -q'" >> /home/${U}/.bashrc
