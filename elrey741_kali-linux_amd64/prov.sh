2782e5275b8f6c29ef96fd7217632783c47ec374
Alex <alex@secureideas.com>
2018-09-20 11:48:02 -0400

    doing full update on boot and adding git settings

--- elrey741_kali-linux_amd64/prov.sh
+++ elrey741_kali-linux_amd64/prov.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+## system
+# getting the most up to date packages from the last package
+apt-get update
+apt-get upgrade -y -o Dpkg::Options::='--force-confnew'
+apt-get dist-upgrade -y -o Dpkg::Options::='--force-confnew'
+
+## metasploit
 # initializing the msf db
 msfdb init
