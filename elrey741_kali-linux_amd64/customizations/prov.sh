e65f3845f485bce8bced1e2e85b41f98716866de
elreydetoda <alex@secureideas.com>
2020-02-17 10:25:24 -0500

    removing sudo because provisions as root

--- elrey741_kali-linux_amd64/customizations/prov.sh
+++ elrey741_kali-linux_amd64/customizations/prov.sh
@@ -8,18 +8,18 @@ bash_prompt="${customization_folder}/bash_prompt"
 
 ## system base
 # getting the most up to date packages from the last package
-sudo apt-get update
-sudo -E apt-get upgrade -y -o Dpkg::Options::='--force-confnew'
-sudo -E apt-get dist-upgrade -y -o Dpkg::Options::='--force-confnew'
+apt-get update
+apt-get upgrade -y -o Dpkg::Options::='--force-confnew'
+apt-get dist-upgrade -y -o Dpkg::Options::='--force-confnew'
 
 ## metasploit
 # initializing the msf db
 msfdb init
 # enabling msf database to start at boot
-sudo systemctl enable postgresql
+systemctl enable postgresql
 
 # starting docker and enabling
-sudo systemctl enable --now docker
+systemctl enable --now docker
 
 if [[ -f "${bash_prompt}" ]] ; then
   cat "${bash_prompt}" >> ~vagrant/.bashrc
